5f5986603a08d97fbca6f7bbd5fdd1ca351b451d
Tieregnimin <tieregnimin@gmail.com>
2025-06-22 02:05:10 +0000

    Nouvelles Modifications de fichiers

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,11 @@
-// frontend/api/axios.js
-import api from "../api/axios";
+// frontend/src/api/axios.js
+import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL; // fallback local
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
-  withCredentials: true, // ✅ important pour cookies cross-domain
+  withCredentials: true,
 });
 
 console.log("✅ Axios baseURL:", api.defaults.baseURL);
